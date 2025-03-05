9e30c9bcb09de732f3e7cccc630fe85b42e65374
jithu2023 <jithumon.j@kalvium.community>
2025-03-05 11:13:43 +0530

    INITIAL

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import  Footer from './Components/Footer/Footer'
-import Home from './Components/Home/Home'
-import Info from './Components/Info/Info'
-import Lounge from './Components/Lounge/Lounge'
-import Search from './Components/Search/Search'
-import Support from './Components/Support/Support'
-import Travelers from './Components/Travelers/Travelers'
-import Subscribers from './Components/Subscribers/Subscribers'
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import  Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Info from './Components/Info/Info';
+import Lounge from './Components/Lounge/Lounge';
+import Search from './Components/Search/Search';
+import Support from './Components/Support/Support';
+import Travelers from './Components/Travelers/Travelers';
+import Subscribers from './Components/Subscribers/Subscribers';
 
 function App() {
   return (
